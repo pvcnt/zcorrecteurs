ea1e2a413cdaac52286965d9ec0f865ec8b0b4a6
Vincent <vincent@zcorrecteurs.fr>
2017-03-06 18:54:48 +0100

    Crée le répertoire web/bundles au lancement.

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -5,6 +5,7 @@ chown -R www-data:www-data /var/log/symfony
 chown -R www-data:www-data /var/cache/symfony
 chown -R www-data:www-data /var/cache/composer
 
+mkdir -p web/bundles && chown -R www-data:www-data web/bundles
 mkdir -p web/compiled && chown -R www-data:www-data web/compiled
 mkdir -p web/uploads && chown -R www-data:www-data web/uploads
 mkdir -p data/index && chown -R www-data:www-data data/index
