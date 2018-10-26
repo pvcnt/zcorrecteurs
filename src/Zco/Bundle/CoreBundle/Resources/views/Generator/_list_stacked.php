9f6f675a3e7f7f35a63512600b09eb6adb88fe8a
Vincent <vincent@zcorrecteurs.fr>
2018-10-26 21:28:29 +0100

    Supprime le code de Generator et Config

--- src/Zco/Bundle/CoreBundle/Resources/views/Generator/_list_stacked.php
+++
@@ -1,3 +0,0 @@
-<td colspan="<?php echo count($fields); ?>">
-	<?php echo $this->replaceReferences($config['list']['params'], $row); ?>
-</td>
--- src/Zco/Bundle/CoreBundle/Resources/views/Generator/_pagination.php
+++
@@ -1,5 +0,0 @@
-<tr>
-	<td colspan="<?php echo $colspan; ?>">
-		Page : <?php echo $paginatorPage->render() ?>
-	</td>
-</tr>
