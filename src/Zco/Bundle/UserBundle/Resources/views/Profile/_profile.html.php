1f0662ba76a1782c19143c4c75e4e4dd7199fcae
Vincent <vincent@zcorrecteurs.fr>
2018-12-13 16:17:09 +0100

    Nettoie quelques CSS et JS

--- src/Zco/Bundle/UserBundle/Resources/views/Profile/_profile.html.php
+++ src/Zco/Bundle/UserBundle/Resources/views/Profile/_profile.html.php
@@ -3,9 +3,7 @@
 		<?php $c = 0; if ($user->hasBirthDate()): ?>
 			<i class="icon-gift"></i>
 			<span class="gris">Âgé de</span>
-			<strong class="popover-link" data-title="<?php echo $user->getAge() ?> ans" data-content="<?php echo htmlspecialchars($user->getUsername()) ?> est né <?php echo $view['humanize']->dateformat($user->getBirthdate(), DATE, MINUSCULE) ?>.">
-				<?php echo $user->getAge() ?> ans
-			</strong>
+			<strong><?php echo $user->getAge() ?> ans</strong>
 		<?php ++$c; endif ?>
 		<?php if ($user->isCountryDisplayed() && $user->hasLocalisation()): ?>
 			<?php if ($c > 0): ?>|<?php endif ?>
@@ -77,4 +75,3 @@
 	</li>
 	<?php endif ?>
 </ul>
-<?php $view['javelin']->initBehavior('popover', array('selector' => '.popover-link')) ?>
