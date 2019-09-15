84aba7fb0b8330539da60c657648db56d6c305d8
Vincent <vincent.primault@gmail.com>
2019-09-15 19:43:47 +0200

    Migre DoctrineOrmTypeGuesser vers Symfony 3

--- src/Zco/Bundle/Doctrine1Bundle/Form/DoctrineOrmTypeGuesser.php
+++ src/Zco/Bundle/Doctrine1Bundle/Form/DoctrineOrmTypeGuesser.php
@@ -21,10 +21,19 @@
 
 namespace Zco\Bundle\Doctrine1Bundle\Form;
 
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormTypeGuesserInterface;
 use Symfony\Component\Form\Guess\Guess;
 use Symfony\Component\Form\Guess\TypeGuess;
 use Symfony\Component\Form\Guess\ValueGuess;
+use Zco\Bundle\Doctrine1Bundle\Form\Type\EntityType;
 
 /**
  * Service en charge de « deviner » le type des champs de formulaire et les
@@ -40,42 +49,42 @@ class DoctrineOrmTypeGuesser implements FormTypeGuesserInterface
     public function guessType($class, $property)
     {
         if (!$metadata = $this->getMetadata($class)) {
-            return new TypeGuess('text', array(), Guess::LOW_CONFIDENCE);
+            return new TypeGuess(TextType::class, array(), Guess::LOW_CONFIDENCE);
         }
 
         if ($metadata->hasRelation($property)) {
             $multiple = $metadata->getRelation($property)->getType() === \Doctrine_Relation::MANY;
             $class = $metadata->getRelation($property)->getClass();
 
-            return new TypeGuess('entity', array('class' => $class, 'multiple' => $multiple), Guess::HIGH_CONFIDENCE);
+            return new TypeGuess(EntityType::class, array('class' => $class, 'multiple' => $multiple), Guess::HIGH_CONFIDENCE);
         }
 
         switch ($metadata->getTypeOf($property)) {
             case 'boolean':
-                return new TypeGuess('checkbox', array(), Guess::HIGH_CONFIDENCE);
+                return new TypeGuess(CheckboxType::class, array(), Guess::HIGH_CONFIDENCE);
             case 'datetime':
             case 'timestamp':
-                return new TypeGuess('datetime', array(), Guess::HIGH_CONFIDENCE);
+                return new TypeGuess(DateTimeType::class, array(), Guess::HIGH_CONFIDENCE);
             case 'date':
-                return new TypeGuess('date', array(), Guess::HIGH_CONFIDENCE);
+                return new TypeGuess(DateType::class, array(), Guess::HIGH_CONFIDENCE);
             case 'decimal':
             case 'float':
-                return new TypeGuess('number', array(), Guess::MEDIUM_CONFIDENCE);
+                return new TypeGuess(NumberType::class, array(), Guess::MEDIUM_CONFIDENCE);
             case 'integer':
             case 'bigint':
             case 'smallint':
-                return new TypeGuess('integer', array(), Guess::MEDIUM_CONFIDENCE);
+                return new TypeGuess(IntegerType::class, array(), Guess::MEDIUM_CONFIDENCE);
             case 'string':
                 $definition = $metadata->getDefinitionOf($property);
                 if ($definition['length']) {
-                    return new TypeGuess('text', array(), Guess::MEDIUM_CONFIDENCE);
+                    return new TypeGuess(TextType::class, array(), Guess::MEDIUM_CONFIDENCE);
                 } else {
-                    return new TypeGuess('textarea', array(), Guess::MEDIUM_CONFIDENCE);
+                    return new TypeGuess(TextareaType::class, array(), Guess::MEDIUM_CONFIDENCE);
                 }
             case 'time':
-                return new TypeGuess('time', array(), Guess::HIGH_CONFIDENCE);
+                return new TypeGuess(TimeType::class, array(), Guess::HIGH_CONFIDENCE);
             default:
-                return new TypeGuess('text', array(), Guess::LOW_CONFIDENCE);
+                return new TypeGuess(TextType::class, array(), Guess::LOW_CONFIDENCE);
         }
     }
 
