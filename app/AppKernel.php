723a7cd85ab03f4d7e4b742d99933e6cca378575
Vincent <vincent.primault@gmail.com>
2019-09-15 16:15:57 +0200

    Supprime le StatsBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -63,7 +63,6 @@ final class AppKernel extends Kernel
             new Zco\Bundle\MpBundle\ZcoMpBundle(),
             new Zco\Bundle\OptionsBundle\ZcoOptionsBundle(),
             new Zco\Bundle\QuizBundle\ZcoQuizBundle(),
-            new Zco\Bundle\StatsBundle\ZcoStatsBundle(),
             new Zco\Bundle\FileBundle\ZcoFileBundle(),
         );
 
