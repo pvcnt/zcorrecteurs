90406af9567b0ef0fd8d25037ba43fe9eebec567
Vincent <vincent.primault@gmail.com>
2020-01-03 23:42:40 +0100

    N'ignore pas le dossier Resources/public/js/vendor lors du rsync

--- build/devbox/update-sources.sh
+++ build/devbox/update-sources.sh
@@ -19,11 +19,11 @@
 rsync -urzvhl /vagrant/ /home/vagrant/zcorrecteurs \
     --exclude=.git \
     --exclude=.gitignore \
-    --exclude=vendor \
-    --exclude=web/bundles \
-    --exclude=web/compiled \
-    --exclude=app/cache \
-    --exclude=app/logs \
+    --exclude=/vendor \
+    --exclude=/web/bundles \
+    --exclude=/web/compiled \
+    --exclude=/app/cache \
+    --exclude=/app/logs \
     --exclude=composer.phar \
     --exclude=.vagrant \
     --exclude=Vagrantfile \
