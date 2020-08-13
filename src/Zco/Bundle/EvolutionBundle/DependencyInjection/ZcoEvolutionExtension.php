7b545dfc1b8301630ff5f3e8f06f2f643290f294
Vincent <vincent.primault@gmail.com>
2020-08-13 18:25:00 +0200

    Corrige ZcoEvolutionExtension

--- src/Zco/Bundle/EvolutionBundle/DependencyInjection/ZcoEvolutionExtension.php
+++ src/Zco/Bundle/EvolutionBundle/DependencyInjection/ZcoEvolutionExtension.php
@@ -22,9 +22,7 @@
 namespace Zco\Bundle\EvolutionBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\Config\FileLocator;
 
 /**
  * ZcoEvolutionExtension.
@@ -41,7 +39,5 @@ class ZcoEvolutionExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-		$loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-		$loader->load('services.yml');
 	}
 }
